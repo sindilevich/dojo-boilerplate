a1319b6258d5ad1c021069fd435a04fe3a5d892b
Matanel Sindilevich <sindilevich@gmail.com>
2017-02-15 22:40:18 +0200

    Windows build script will clean old files and process Stylus stylesheets

--- build.cmd
+++ build.cmd
@@ -1,1 +1,17 @@
-call src\util\buildscripts\build.bat --profile profiles\app.profile.js --releaseDir dist > results.log 2>&1
+@echo off
+
+rem Destination directory for built code
+set DISTDIR=dist
+
+echo Building application with "%PROFILE%" to "%DISTDIR%".
+
+if exist "%DISTDIR%" (
+	echo | set /p=Cleaning old files...
+	rmdir /s /q "%DISTDIR%"
+	echo  done
+)
+
+call node_modules\.bin\stylus.cmd "src\app\resources\app.styl"
+call src\util\buildscripts\build.bat --profile profiles\app.profile.js --releaseDir ..\%DISTDIR% > results.log 2>&1
+
+echo Build complete
